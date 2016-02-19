190529f304af7d56955eaede3b158aa918f1c7df
B123MILLER <b215miller@yahoo.com>
2016-02-19 03:59:37 +0000

    nitrous-backup at Fri Feb 19 03:59:37 UTC 2016

--- variable.rb
+++ variable.rb
@@ -1,2 +1,5 @@
-greeting = "Hello World" # Define a local variable called 'greeting',
- # give it the value of the string "Hello World"
+def greeting (greeting ="Hello World")
+  puts greeting # Code your solution in variable.rb
+end
+end
+
