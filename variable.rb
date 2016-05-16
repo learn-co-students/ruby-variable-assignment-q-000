61976fd2f39854d97a7a8c0065b848d089df60fd
Michael  Lopez <mlopezbiller@gmail.com>
2016-05-16 12:20:32 -0400

    Done.

--- variable.rb
+++ variable.rb
@@ -1,2 +1,2 @@
- # Define a local variable called 'greeting',
- # give it the value of the string "Hello World"
+greeting = "hello World"
+greeting = get_variable_from_file(./variable.rb, "greeting")
