92863861592476a54f5c08e744b245b9495633da
David Carranza <david.carranza777@gmail.com>
2016-05-31 12:29:09 -0500

    Done.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,16 +1,16 @@
 require_relative '../variable.rb'
-
+greeting="Hello World"
 RSpec.configure do |config|
   config.order = 'default'
 end
 
-def get_variable_from_file(file, variable)
+def get_variable_from_file(spec_helper.rb, greeting)
   file_scope = binding
   file_scope.eval(File.read(file))
 
   begin
-    return file_scope.local_variable_get(variable)
+    return file_scope.local_variable_get(greeting)
   rescue NameError
-    raise NameError, "local variable `#{variable}` not defined in #{file}."
+    raise NameError, "local variable `#{greeting}` not defined in #{spec_helper.rb}."
   end
 end
